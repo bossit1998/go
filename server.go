fdfaa018deb9791a7643cf8f7a66bcccb8bfe376
abdubosit <abduw_1806@mail.ru>
2020-09-14 10:14:39 +0500

    fix port number in server.go

--- server.go
+++ server.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	lis, err := net.Listen("tcp", "9000")
+	lis, err := net.Listen("tcp", ":9000")
 	if err != nil {
 		log.Fatalf("Failed to listen on port 9000: %v", err)
 	}
